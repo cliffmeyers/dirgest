2f40002427e658e8a40c381044e6a0586b416049
Cliff Meyers <cliff.meyers@gmail.com>
2019-12-08 08:32:45 -0500

    add fs / memfs testing support

--- src/dirgest.spec.ts
+++ src/dirgest.spec.ts
@@ -1,10 +1,20 @@
 import * as path from 'path';
 
-import { dirgest } from './dirgest';
+import { vol } from 'memfs';
+
+import { makeDirgest } from './dirgest';
 
 describe('dirgest', () => {
     it('should work', (done) => {
-        dirgest(path.resolve(__dirname, './__mocks__'), 'sha1', (err, hashes) => {
+        const mockfs = {
+            'a.js': 'foo',
+            'b.js': 'bar'
+        };
+        vol.fromJSON(mockfs);
+
+        const dirgest = makeDirgest(vol as any);
+
+        dirgest('.', 'sha1', (err, hashes) => {
             console.log('error!', err)
             console.log(JSON.stringify(hashes, null, 2));
             done();
