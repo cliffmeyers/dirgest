9b521f9274c258dd5613462b8a8c1fffc71aef4c
Cliff Meyers <cliff.meyers@gmail.com>
2019-12-08 07:45:03 -0500

    remove json schema validation test code

--- src/dirgest.spec.ts
+++ src/dirgest.spec.ts
@@ -1,6 +1,4 @@
-import * as fs from 'fs';
 import * as path from 'path';
-import AJV  from 'ajv';
 
 import { dirgest } from './dirgest';
 
@@ -11,11 +9,5 @@ describe('dirgest', () => {
             console.log(JSON.stringify(hashes, null, 2));
             done();
         });
-    });
-    it('should validate json', () => {
-        const ajv = new AJV();
-        const schema = JSON.parse(fs.readFileSync(path.resolve(__dirname, './__mocks__/dir-hash.schema.json')).toString());
-        const valid8 = ajv.compile(schema);
-        console.log(valid8({ hash: 'foo', files: {}}));
     });
 });
