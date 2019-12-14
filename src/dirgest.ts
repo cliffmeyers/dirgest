103787b6134283cd980b74e8d9ed8f0ca8bbac5e
Cliff Meyers <cliff.meyers@gmail.com>
2019-12-13 23:30:52 -0500

    port to promises; tests passing

--- src/dirgest.ts
+++ src/dirgest.ts
@@ -3,13 +3,8 @@
 
 import crypto from 'crypto';
 import fs from 'fs';
-// import * as path from 'path';
 
 import { DirHash } from './dir-hash';
-
-interface Callback {
-    (err: any, dirgest?: DirHash) :void
-}
 
 interface Filter {
   shouldDigest(path: string, isDir: boolean): boolean;
@@ -29,56 +24,58 @@ export class Dirgest {
     this._filesystem = filesystem || fs;
   }
   
-  dirgest(root: string, callback: Callback, filter?: Filter) {
+  dirgest(root: string, filter?: Filter): Promise<DirHash> {
     if (!root || typeof(root) !== 'string') {
       throw new TypeError('root is required (string)');
     }
-    if (!callback || typeof(callback) !== 'function') {
-      throw new TypeError('callback is required (function)');
-    }
-  
-    const hashes: any = {};
-  
-    this._filesystem.readdir(root, { withFileTypes: true }, (err, files) => {
-      if (err) { console.log(err); return callback(err); }
-  
-      if (files.length === 0) {
-        return callback(undefined, {hash: '', files: {}});
-      }
-  
-      let hashed = 0;
-      files.forEach((dirent) => {
-        const { name } = dirent;
-        const currentPath = root + '/' + name;
-        if (dirent.isDirectory()) {
-          return this.dirgest(currentPath, (errDirgest, hash) => {
-            if (errDirgest) return hash;
 
+    return new Promise((resolve, reject) => {
+      const hashes: any = {};
+  
+      this._filesystem.readdir(root, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          reject(err)
+        }
+    
+        if (files.length === 0) {
+          resolve({hash: '', files: {}});
+        }
+    
+        let hashed = 0;
+
+        files.forEach(async (dirent) => {
+          const { name } = dirent;
+          const currentPath = root + '/' + name;
+          
+          if (dirent.isDirectory()) {
+            const hash = await this.dirgest(currentPath);
             hashes[name] = hash;
             if (++hashed >= files.length) {
-              return callback(undefined, this._summarize(hashes));
+              resolve(this._summarize(hashes));
             }
-          });
-        } else if (dirent.isFile()) {
-          this._filesystem.readFile(currentPath, 'utf8', (errRead, data) => {
-            if (errRead) return callback(errRead);
-
-            const hash = crypto.createHash(this._method);
-            hash.update(data);
-            hashes[name] = hash.digest('hex');
-
-            if (++hashed >= files.length) {
-              return callback(undefined, this._summarize(hashes));
+          } else if (dirent.isFile()) {
+            this._filesystem.readFile(currentPath, 'utf8', (errRead, data) => {
+              if (errRead) {
+                reject(errRead);
+              }
+  
+              const hash = crypto.createHash(this._method);
+              hash.update(data);
+              hashes[name] = hash.digest('hex');
+  
+              if (++hashed >= files.length) {
+                resolve(this._summarize(hashes));
+              }
+            });
+          } else {
+            console.error('Skipping hash of %s', name);
+            if (++hashed > files.length) {
+              resolve(this._summarize(hashes));
             }
-          });
-        } else {
-          console.error('Skipping hash of %s', name);
-          if (++hashed > files.length) {
-            return callback(undefined, this._summarize(hashes));
           }
-        }
+        });
       });
-    });
+    })  
   }
 
   _summarize(hashes: any) {
