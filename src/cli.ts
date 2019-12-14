80cfce27ca1696b8b69414e925aca53c07e20e7e
Cliff Meyers <cliff.meyers@gmail.com>
2019-12-13 23:40:37 -0500

    fix compile error in cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 
 import { Dirgest } from './dirgest';
-import { DirHash } from './dir-hash';
 
 const dirPath = process.argv[2];
 const method = process.argv[3]
 
 new Dirgest(method || undefined)
-    .dirgest(dirPath, (error: Error, result: DirHash | undefined) => {
-        if (error) {
-            console.error(error);
-            process.exit(1);
-        }
+    .dirgest(dirPath)
+    .then(result => {
         console.log(JSON.stringify(result, null, 4));
-    })
+    });
